469a5057e8272b96568fd4ba0fd5dd44b84bae64
Ping Chen <remotenonsense@gmail.com>
2025-08-11 08:34:37 +0900

    v0.1.4: different version code for each abi

--- plugins/withAndroidPlugin.ts
+++ plugins/withAndroidPlugin.ts
@@ -4,14 +4,20 @@ import { withAppBuildGradle } from '@expo/config-plugins/build/plugins/android-p
 const withAndroidSigningConfig: ConfigPlugin = (config) => {
   return withAppBuildGradle(config, (config) => {
     // https://www.reddit.com/r/expo/comments/1j4v323/comment/mit9b2a/
-    config.modResults.contents = config.modResults.contents.replace(
-      /buildTypes \{([\s\S]*?)release \{([\s\S]*?)signingConfig signingConfigs\.debug/,
-      `buildTypes {$1release { `,
-    )
+    config.modResults.contents = config.modResults.contents
+      .replace(
+        'android {',
+        `ext.abiCodes = [x86:1, x86_64:2, 'armeabi-v7a':3, 'arm64-v8a': 4]
 
-    config.modResults.contents = config.modResults.contents.replace(
-      /androidResources \{([\s\S]*?)}/,
-      `androidResources {$1}
+android {`,
+      )
+      .replace(
+        /buildTypes \{([\s\S]*?)release \{([\s\S]*?)signingConfig signingConfigs\.debug/,
+        `buildTypes {$1release { `,
+      )
+      .replace(
+        /androidResources \{([\s\S]*?)}/,
+        `androidResources {$1}
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
@@ -21,10 +27,18 @@ const withAndroidSigningConfig: ConfigPlugin = (config) => {
             reset()
             enable true
             universalApk false
-            include "armeabi-v7a", "arm64-v8a", "x86", "x86_64"
+            include project.ext.abiCodes.keySet() as String[]
+        }
+    }
+    android.applicationVariants.configureEach { variant ->
+        variant.outputs.each { output ->
+            def baseAbiVersionCode = project.ext.abiCodes.get(output.getFilter(com.android.build.OutputFile.ABI))
+            if (baseAbiVersionCode != null) {
+                output.versionCodeOverride = (100 * project.android.defaultConfig.versionCode) + baseAbiVersionCode
+            }
         }
     }`,
-    )
+      )
 
     return config
   })
