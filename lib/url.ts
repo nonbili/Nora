a6433de590a436f9d4cb71fe6163b565fb2e3f5e
Ping Chen <remotenonsense@gmail.com>
2026-08-18 14:29:48 +0900

    expand tracking params removed from urls, fix #324

--- lib/url.ts
+++ lib/url.ts
@@ -1,5 +1,7 @@
 const trackingParams = [
   // fb
+  'fbclid',
+  'mibextid',
   'referral_source',
   'surface_type',
   // instagram & reddit
@@ -8,10 +10,39 @@ const trackingParams = [
   'utm_name',
   'utm_term',
   'utm_content',
+  // reddit
+  'share_id',
+  'rdt',
   // instagram
   'igsh',
+  'igshid',
   // threads
   'xmt',
+  // tiktok
+  '_t',
+  '_r',
+  // twitter/x
+  'twclid',
+  // google
+  'gclid',
+  'gbraid',
+  'wbraid',
+  // microsoft/bing
+  'msclkid',
+  // mailchimp
+  'mc_cid',
+  'mc_eid',
+  // hubspot
+  '_hsenc',
+  '_hsmi',
+  // yandex
+  'yclid',
+  // generic
+  'utm_id',
+  'utm_campaign',
+  'utm_referrer',
+  'ref_src',
+  'ref_url',
 ]
 
 export function removeTrackingParams(v: string) {
